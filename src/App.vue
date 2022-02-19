32227a50b8e3e2921df26d45de43ce7c17456d1d
MaK1aren <makarsmirnov2@gmail.com>
2022-02-19 15:35:49 +0300

    new project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<StarFinger/>
+</template>
+
+<script>
+import StarFinger from "@/components/StarFinger.vue"
+
+export default {
+  name: 'App',
+  components: {
+    StarFinger
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
